4811b76788a0923d5d1da8ad0ae62d0741d41897
Alexander Ivkin <a@ivkin.dev>
2025-12-16 18:51:58 +0100

    Blog post about rate limiting at winrate

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import mermaid from 'astro-mermaid';
 
 export default defineConfig({
   site: 'https://ivkin.dev',
@@ -10,6 +11,7 @@ export default defineConfig({
     mdx(),
     sitemap(),
     tailwind(),
+    mermaid(),
   ],
   markdown: {
     shikiConfig: {
